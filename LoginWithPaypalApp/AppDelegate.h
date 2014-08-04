917b6aefb88f003a0edd56a554b240ceddb57cd4
Mermache <mermache101@gmail.com>
2014-08-04 14:27:50 -0700

    Initial Commit

---
+++ LoginWithPaypalApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LoginWithPaypalApp
+//
+//  Created by Mermache, Abdull(AWF) on 8/4/14.
+//  Copyright (c) 2014 Ebay.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
